9bb883e1a2a5a10f634cb07fdcd83cbcd81b3bb9
Paul Steele <your-email@example.com>
2025-07-07 14:10:19 -0700

    Removed all error_log statements from save_acf_fields to resolve critical error.

--- tribe-acf-frontend.php
+++ tribe-acf-frontend.php
@@ -164,16 +164,12 @@ class Tribe_ACF_Frontend {
             return;
         }
 
-        error_log( 'Tribe ACF Frontend: save_acf_fields function called for post_id: ' . $post_id );
-
         // Check if ACF has data to save for this post.
         if ( ! empty( $_POST['acf'] ) ) {
-            error_log( 'Tribe ACF Frontend: $_POST['acf'] is NOT empty.' );
-            error_log( var_export( $_POST['acf'], true ) );
             // ACF should automatically save fields if the field group is assigned to the post type.
             // We are not calling acf_form_submit() here as Community Events handles its own form submission.
         } else {
-            error_log( 'Tribe ACF Frontend: $_POST['acf'] IS empty.' );
+            // No ACF data in POST.
         }
     }
 }
